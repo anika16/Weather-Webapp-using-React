6f6fdfecfb9a13e7cfb78f361ef49a69b3bdb718
Anika Kalia <anika203a@gmail.com>
2021-07-13 20:49:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'; 
+import React, { Component }  from 'react';
+
+import MainDiv from './components/MainDiv';
+
+function App() {
+  return (
+    <div className="App">
+      <MainDiv /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style/footer.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+         
+        </div>
+    )
+}
+
+export default Footer
